cad49291095589020368e8e1d7882ec1a672a7f1
Mohamed <elkhamlichim099@gmail.com>
2026-08-04 14:46:51 +0100

    fix: update sitemap change frequency to daily

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://rawafid.tech",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "daily",
       priority: 1,
     },
   ];
